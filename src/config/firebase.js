280b342272aec08dd3a8c7792cb93b272467a68a
aguspranyoto <agusprnyt@gmail.com>
2023-10-03 08:12:50 +0700

    website is under construction

---
+++ src/config/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAvGla3FM9rjNZrjslrb9FWqxSXPZ3zaRc",
+
+  authDomain: "agus-pranyoto.firebaseapp.com",
+
+  projectId: "agus-pranyoto",
+
+  storageBucket: "agus-pranyoto.appspot.com",
+
+  messagingSenderId: "959507230465",
+
+  appId: "1:959507230465:web:a84a9a2ce72654d1c6b500",
+
+  measurementId: "G-JG41GR3G4M",
+};
+
+const app = initializeApp(firebaseConfig);
